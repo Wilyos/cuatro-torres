1d5f9b793746211f28b0b024229c12296657b238
Wil Bohorquez <wilyos99@gmail.com>
2025-07-03 13:53:09 -0500

    snew changes

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,14 +1,14 @@
 import { motion } from "framer-motion";
 
 const bannersH = [
-  "BANNER 1 H.png",
-  "BANNER 2 H.png",
-  "BANNER 3 H.png",
+  "BANNER-1-H.png",
+  "BANNER-2-H.png",
+  "BANNER-3-H.png",
 ];
 
 const bannersV = [
-  "BANNER 1 V.png",
-  "BANNER 2 V .png",
+  "BANNER-1-V.png",
+  "BANNER-2-V .png",
 ];
 
 const scrollToContacto = () => {
