d6764c8ae91e3bd5fd245ec70f3c2f326eda58b1
Wil Bohorquez <wilyos99@gmail.com>
2025-07-03 14:23:50 -0500

    cambio en colores

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline"
 
 const HeroSection = () => {
   return (
-    <section className='relative min-h-[300px] md:h-screen bg-gradient-to-b from-blue-800 to-blue-800 flex xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 overflow-hidden pb-2 mb-5 md:pb-2'>
+    <section className='relative min-h-[300px] md:h-screen bg-gradient-to-b from-blue-950 to-blue-900 flex xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 overflow-hidden pb-2 mb-5 md:pb-2'>
       {/* Spline solo en escritorio/tablet */}
       <div className="hidden md:block w-full h-full absolute xl:right-[0%] right-0 top-[-20%] lg:top-0 sm:right-[-25%]">
         <Spline
